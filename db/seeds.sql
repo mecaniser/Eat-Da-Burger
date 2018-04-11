2ee4361b5ade3595bdba7ead7b21a10fbbf935fa
sergio <sergioburca@gmail.com>
2018-04-11 17:43:26 -0400

    update

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,8 +1,9 @@
 -- Seed data for the DB that will be created 
 
-INSERT INTO burgers (brgr_name, devoured) VALUES ("Ultimate Bacon Burger",false);
-INSERT INTO burgers (brgr_name, devoured) VALUES ("Sunrise Burger",false);
-INSERT INTO burgers (brgr_name, devoured) VALUES ("Southern Smokehouse Burger",false);
-INSERT INTO burgers (brgr_name, devoured) VALUES ("Big Mouth® Bites",true);
-INSERT INTO burgers (brgr_name, devoured) VALUES ("Classic Bacon Burger",true);
-INSERT INTO burgers (brgr_name, devoured) VALUES ("Guacamole Burger",true);
+INSERT INTO burgers (brgr_name, devoured) 
+VALUES ("Ultimate Bacon Burger",false),
+ ("Sunrise Burger",false),
+ ("Southern Smokehouse Burger",false),
+ ("Big Mouth® Bites",true),
+ ("Classic Bacon Burger",true),
+ ("Guacamole Burger",true);
